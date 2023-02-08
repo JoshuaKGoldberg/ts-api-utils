ce521cf23ff958285218e64ff8063ad2690a5ec4
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-02-08 17:42:55 +1300

    feat: add compound type gaurds from tsutils

--- src/nodes/typeGuards/compound.ts
+++ src/nodes/typeGuards/compound.ts
@@ -3,12 +3,125 @@
 
 import * as ts from "typescript";
 
+import {
+	isModuleDeclaration,
+	isPropertyAccessExpression,
+	isTypeReferenceNode,
+} from "./simple";
+
+export function isAccessorDeclaration(
+	node: ts.Node
+): node is ts.AccessorDeclaration {
+	return (
+		node.kind === ts.SyntaxKind.GetAccessor ||
+		node.kind === ts.SyntaxKind.SetAccessor
+	);
+}
+
+export function isAssertionExpression(
+	node: ts.Node
+): node is ts.AssertionExpression {
+	return (
+		node.kind === ts.SyntaxKind.AsExpression ||
+		node.kind === ts.SyntaxKind.TypeAssertionExpression
+	);
+}
+
+export function isBindingPattern(node: ts.Node): node is ts.BindingPattern {
+	return (
+		node.kind === ts.SyntaxKind.ArrayBindingPattern ||
+		node.kind === ts.SyntaxKind.ObjectBindingPattern
+	);
+}
+
+export function isBlockLike(node: ts.Node): node is ts.BlockLike {
+	switch (node.kind) {
+		case ts.SyntaxKind.SourceFile:
+		case ts.SyntaxKind.Block:
+		case ts.SyntaxKind.ModuleBlock:
+		case ts.SyntaxKind.CaseClause:
+		case ts.SyntaxKind.DefaultClause:
+			return true;
+		default:
+			return false;
+	}
+}
+
+export function isBooleanLiteral(node: ts.Node): node is ts.BooleanLiteral {
+	return (
+		node.kind === ts.SyntaxKind.TrueKeyword ||
+		node.kind === ts.SyntaxKind.FalseKeyword
+	);
+}
+
+export function isBreakOrContinueStatement(
+	node: ts.Node
+): node is ts.BreakOrContinueStatement {
+	return (
+		node.kind === ts.SyntaxKind.BreakStatement ||
+		node.kind === ts.SyntaxKind.ContinueStatement
+	);
+}
+
+export function isCallLikeExpression(
+	node: ts.Node
+): node is ts.CallLikeExpression {
+	switch (node.kind) {
+		case ts.SyntaxKind.CallExpression:
+		case ts.SyntaxKind.Decorator:
+		case ts.SyntaxKind.JsxOpeningElement:
+		case ts.SyntaxKind.JsxSelfClosingElement:
+		case ts.SyntaxKind.NewExpression:
+		case ts.SyntaxKind.TaggedTemplateExpression:
+			return true;
+		default:
+			return false;
+	}
+}
+
+export function isCaseOrDefaultClause(
+	node: ts.Node
+): node is ts.CaseOrDefaultClause {
+	return (
+		node.kind === ts.SyntaxKind.CaseClause ||
+		node.kind === ts.SyntaxKind.DefaultClause
+	);
+}
+
+export function isClassLikeDeclaration(
+	node: ts.Node
+): node is ts.ClassLikeDeclaration {
+	return (
+		node.kind === ts.SyntaxKind.ClassDeclaration ||
+		node.kind === ts.SyntaxKind.ClassExpression
+	);
+}
+
+export type ConstAssertionExpression = ts.AssertionExpression & {
+	type: ts.TypeReferenceNode;
+	typeName: ConstAssertionIdentifier;
+};
+
+export type ConstAssertionIdentifier = ts.Identifier & {
+	escapedText: ts.__String & "const";
+};
+
+export function isConstAssertionExpression(
+	node: ts.AssertionExpression
+): node is ConstAssertionExpression {
+	return (
+		isTypeReferenceNode(node.type) &&
+		node.type.typeName.kind === ts.SyntaxKind.Identifier &&
+		node.type.typeName.escapedText === "const"
+	);
+}
+
 export function isEntityNameExpression(
 	node: ts.Node
 ): node is ts.EntityNameExpression {
 	return (
 		node.kind === ts.SyntaxKind.Identifier ||
-		(ts.isPropertyAccessExpression(node) &&
+		(isPropertyAccessExpression(node) &&
 			isEntityNameExpression(node.expression))
 	);
 }
@@ -65,6 +178,67 @@ export function isExpression(node: ts.Node): node is ts.Expression {
 	}
 }
 
+export function isForInOrOfStatement(
+	node: ts.Node
+): node is ts.ForInOrOfStatement {
+	return (
+		node.kind === ts.SyntaxKind.ForOfStatement ||
+		node.kind === ts.SyntaxKind.ForInStatement
+	);
+}
+
+export function isIterationStatement(
+	node: ts.Node
+): node is ts.IterationStatement {
+	switch (node.kind) {
+		case ts.SyntaxKind.ForStatement:
+		case ts.SyntaxKind.ForOfStatement:
+		case ts.SyntaxKind.ForInStatement:
+		case ts.SyntaxKind.WhileStatement:
+		case ts.SyntaxKind.DoStatement:
+			return true;
+		default:
+			return false;
+	}
+}
+
+export function isJsxAttributeLike(node: ts.Node): node is ts.JsxAttributeLike {
+	return (
+		node.kind === ts.SyntaxKind.JsxAttribute ||
+		node.kind === ts.SyntaxKind.JsxSpreadAttribute
+	);
+}
+
+export function isJsxOpeningLikeElement(
+	node: ts.Node
+): node is ts.JsxOpeningLikeElement {
+	return (
+		node.kind === ts.SyntaxKind.JsxOpeningElement ||
+		node.kind === ts.SyntaxKind.JsxSelfClosingElement
+	);
+}
+
+export function isLiteralExpression(
+	node: ts.Node
+): node is ts.LiteralExpression {
+	return (
+		node.kind >= ts.SyntaxKind.FirstLiteralToken &&
+		node.kind <= ts.SyntaxKind.LastLiteralToken
+	);
+}
+
+export function isNamespaceDeclaration(
+	node: ts.Node
+): node is ts.NamespaceDeclaration {
+	return (
+		isModuleDeclaration(node) &&
+		node.name.kind === ts.SyntaxKind.Identifier &&
+		node.body !== undefined &&
+		(node.body.kind === ts.SyntaxKind.ModuleBlock ||
+			isNamespaceDeclaration(node.body))
+	);
+}
+
 export type NumericOrStringLikeLiteral =
 	| ts.NumericLiteral
 	| ts.StringLiteral
@@ -83,21 +257,42 @@ export function isNumericOrStringLikeLiteral(
 	}
 }
 
-export type ConstAssertionExpression = ts.AssertionExpression & {
-	type: ts.TypeReferenceNode;
-	typeName: ConstAssertionIdentifier;
-};
+export function isSignatureDeclaration(
+	node: ts.Node
+): node is ts.SignatureDeclaration {
+	switch (node.kind) {
+		case ts.SyntaxKind.CallSignature:
+		case ts.SyntaxKind.ConstructSignature:
+		case ts.SyntaxKind.MethodSignature:
+		case ts.SyntaxKind.IndexSignature:
+		case ts.SyntaxKind.FunctionType:
+		case ts.SyntaxKind.ConstructorType:
+		case ts.SyntaxKind.JSDocFunctionType:
+		case ts.SyntaxKind.FunctionDeclaration:
+		case ts.SyntaxKind.MethodDeclaration:
+		case ts.SyntaxKind.Constructor:
+		case ts.SyntaxKind.GetAccessor:
+		case ts.SyntaxKind.SetAccessor:
+		case ts.SyntaxKind.FunctionExpression:
+		case ts.SyntaxKind.ArrowFunction:
+			return true;
+		default:
+			return false;
+	}
+}
 
-export type ConstAssertionIdentifier = ts.Identifier & {
-	escapedText: ts.__String & "const";
-};
-
-export function isConstAssertionExpression(
-	node: ts.AssertionExpression
-): node is ConstAssertionExpression {
+export function isTemplateLiteral(node: ts.Node): node is ts.TemplateLiteral {
 	return (
-		ts.isTypeReferenceNode(node.type) &&
-		node.type.typeName.kind === ts.SyntaxKind.Identifier &&
-		node.type.typeName.escapedText === "const"
+		node.kind === ts.SyntaxKind.TemplateExpression ||
+		node.kind === ts.SyntaxKind.NoSubstitutionTemplateLiteral
+	);
+}
+
+export function isTextualLiteral(
+	node: ts.Node
+): node is ts.StringLiteral | ts.NoSubstitutionTemplateLiteral {
+	return (
+		node.kind === ts.SyntaxKind.StringLiteral ||
+		node.kind === ts.SyntaxKind.NoSubstitutionTemplateLiteral
 	);
 }
