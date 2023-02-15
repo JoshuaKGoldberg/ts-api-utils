58728d56b6a861f33d6ec8f8b522adbdb6830b37
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-02-15 15:07:14 +1300

    chore: remove commented out functions

--- src/types/typeGuards/objects.ts
+++ src/types/typeGuards/objects.ts
@@ -43,25 +43,3 @@ export function isTupleType(type: ts.Type): type is ts.TupleType {
 export function isTypeReference(type: ts.Type): type is ts.TypeReference {
 	return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.Reference);
 }
-
-/*
- * No TypeScript defined type exists for types with these object flags.
- */
-
-// export function isAnonymousType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.Anonymous); }
-// export function isArrayLiteralType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.ArrayLiteral); }
-// export function isClassOrInterfaceType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.ClassOrInterface); }
-// export function isClassType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.Class); }
-// export function isContainsSpreadType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.ContainsSpread); }
-// export function isFreshLiteralType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.FreshLiteral); }
-// export function isInstantiatedType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.Instantiated); }
-// export function isInstantiationExpressionTypeType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.InstantiationExpressionType); }
-// export function isInterfaceType2(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.Interface); }
-// export function isJSLiteralType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.JSLiteral); }
-// export function isJsxAttributesType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.JsxAttributes); }
-// export function isMappedType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.Mapped); }
-// export function isObjectLiteralPatternWithComputedPropertiesType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.ObjectLiteralPatternWithComputedProperties); }
-// export function isObjectLiteralType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.ObjectLiteral); }
-// export function isObjectRestTypeType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.ObjectRestType); }
-// export function isReferenceType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.Reference); }
-// export function isReverseMappedType(type: ts.Type): boolean { return isObjectType(type) && isObjectFlagSet(type, ts.ObjectFlags.ReverseMapped); }
--- src/types/typeGuards/simple.ts
+++ src/types/typeGuards/simple.ts
@@ -93,37 +93,3 @@ export function isUniqueESSymbolType(
 ): type is ts.UniqueESSymbolType {
 	return isTypeFlagSet(type, ts.TypeFlags.UniqueESSymbol);
 }
-
-/*
- * No TypeScript defined type exists for types with these type flags.
- */
-
-// export function isAnyType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.Any); }
-// export function isBigIntLikeType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.BigIntLike); }
-// export function isBigIntType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.BigInt); }
-// export function isBooleanLikeType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.BooleanLike); }
-// export function isBooleanLiteralType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.BooleanLiteral); }
-// export function isBooleanType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.Boolean); }
-// export function isEnumLikeType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.EnumLike); }
-// export function isEnumLiteralType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.EnumLiteral); }
-// export function isESSymbolLikeType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.ESSymbolLike); }
-// export function isESSymbolType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.ESSymbol); }
-// export function isInstantiableNonPrimitiveType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.InstantiableNonPrimitive); }
-// export function isInstantiablePrimitiveType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.InstantiablePrimitive); }
-// export function isNarrowableType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.Narrowable); }
-// export function isNeverType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.Never); }
-// export function isNonPrimitiveType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.NonPrimitive); }
-// export function isNullType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.Null); }
-// export function isNumberLikeType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.NumberLike); }
-// export function isNumberType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.Number); }
-// export function isPossiblyFalsyType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.PossiblyFalsy); }
-// export function isStringLikeType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.StringLike); }
-// export function isStringOrNumberLiteralType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.StringOrNumberLiteral); }
-// export function isStringType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.String); }
-// export function isStructuredOrInstantiableType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.StructuredOrInstantiable); }
-// export function isStructuredTypeType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.StructuredType); }
-// export function isUndefinedType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.Undefined); }
-// export function isUnitType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.Unit); }
-// export function isUnknownType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.Unknown); }
-// export function isVoidLikeType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.VoidLike); }
-// export function isVoidType(type: ts.Type): boolean { return isTypeFlagSet(type, ts.TypeFlags.Void); }
