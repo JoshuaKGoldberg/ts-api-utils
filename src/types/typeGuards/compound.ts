f384decd8b27ad64b4924f2021d53119994268bb
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-02-21 00:39:43 +1300

    fix: make isBooleanLiteralType a type guard

--- src/types/typeGuards/compound.ts
+++ src/types/typeGuards/compound.ts
@@ -1,6 +1,103 @@
 import * as ts from "typescript";
 
+import { isTypeFlagSet } from "../../flags.js";
 import { isTupleType, isTypeReference } from "./objects.js";
+
+/**
+ * `LiteralType` from typescript except that it allows for it to work on arbitrary types.
+ *
+ * @category Type Types
+ */
+export interface GeneralLiteralType extends ts.Type {
+	freshType: GeneralLiteralType;
+	regularType: GeneralLiteralType;
+	value: unknown;
+}
+
+/**
+ * A boolean literal.
+ *
+ * @category Type Types
+ */
+export interface BooleanLiteralType extends GeneralLiteralType {
+	value: boolean;
+}
+
+/**
+ * A "true" literal.
+ *
+ * @category Type Types
+ */
+
+export interface TrueLiteralType extends BooleanLiteralType {
+	value: true;
+}
+
+/**
+ * A "false" literal.
+ *
+ * @category Type Types
+ */
+export interface FalseLiteralType extends BooleanLiteralType {
+	value: false;
+}
+
+/**
+ * Determines whether the given type is a boolean literal type.
+ *
+ * @category Types - Utilities
+ */
+export function isBooleanLiteralType(type: ts.Type): type is BooleanLiteralType;
+
+/**
+ * Determines whether the given type is a boolean literal type for "true".
+ *
+ * @category Types - Utilities
+ */
+export function isBooleanLiteralType(
+	type: ts.Type,
+	literal: true
+): type is TrueLiteralType;
+
+/**
+ * Determines whether the given type is a boolean literal type for "false".
+ *
+ * @category Types - Utilities
+ */
+export function isBooleanLiteralType(
+	type: ts.Type,
+	literal: false
+): type is FalseLiteralType;
+
+export function isBooleanLiteralType(
+	type: ts.Type,
+	literal?: boolean
+): boolean {
+	return (
+		isTypeFlagSet(type, ts.TypeFlags.BooleanLiteral) &&
+		(literal === undefined ||
+			(type as unknown as { intrinsicName: string }).intrinsicName ===
+				(literal ? "true" : "false"))
+	);
+}
+
+/**
+ * Determines whether the given type is a boolean literal type for "true".
+ *
+ * @category Types - Utilities
+ */
+export function isTrueLiteralType(type: ts.Type): type is TrueLiteralType {
+	return isBooleanLiteralType(type, true);
+}
+
+/**
+ * Determines whether the given type is a boolean literal type for "false".
+ *
+ * @category Types - Utilities
+ */
+export function isFalseLiteralType(type: ts.Type): type is FalseLiteralType {
+	return isBooleanLiteralType(type, false);
+}
 
 export function isTupleTypeReference(
 	type: ts.Type
--- src/types/utilities.ts
+++ src/types/utilities.ts
@@ -17,6 +17,7 @@ import {
 import { isNumericPropertyName } from "../syntax.js";
 import { getPropertyOfType } from "./getters.js";
 import {
+	isBooleanLiteralType,
 	isIntersectionType,
 	isLiteralType,
 	isObjectType,
@@ -24,20 +25,13 @@ import {
 	isUnionType,
 } from "./typeGuards/index.js";
 
-/** Determines whether the given type is a boolean literal type and matches the given boolean literal. */
-export function isBooleanLiteralType(type: ts.Type, literal: boolean): boolean {
-	return (
-		isTypeFlagSet(type, ts.TypeFlags.BooleanLiteral) &&
-		(type as unknown as { intrinsicName: string }).intrinsicName ===
-			(literal ? "true" : "false")
-	);
-}
-
 /** Determines whether a type is definitely falsy. This function doesn't unwrap union types. */
 export function isFalsyType(type: ts.Type): boolean {
 	if (
-		type.flags &
-		(ts.TypeFlags.Undefined | ts.TypeFlags.Null | ts.TypeFlags.Void)
+		isTypeFlagSet(
+			type,
+			ts.TypeFlags.Undefined | ts.TypeFlags.Null | ts.TypeFlags.Void
+		)
 	)
 		return true;
 	if (isLiteralType(type)) return !type.value;
