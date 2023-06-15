e11775eb3df2886984fe24dbb0f8fdb7f638edfd
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2023-06-15 10:01:04 -0400

    chore: added an assortment of unit tests (#205)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: starts on #7, #8, #9, #10, #11,
    #12, #13, #14
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/ts-api-utils/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/ts-api-utils/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    I had some time on a road trip today and was motivated to backfill some
    tests 😄. Doesn't add in 100% coverage on any file or really finish any
    of the linked issues. Just raises test coverage across the board in
    general.
    
    Co-authored-by: Rebecca Stevens <rebecca.stevens@outlook.co.nz>

--- src/nodes/typeGuards/union.test.ts
+++ src/nodes/typeGuards/union.test.ts
@@ -1,7 +1,86 @@
+import ts from "typescript";
 import { describe, expect, it } from "vitest";
 
 import { createNode } from "../../test/utils";
-import { isEntityNameExpression } from "./union";
+import {
+	isAccessExpression,
+	isAccessibilityModifier,
+	isArrayBindingOrAssignmentPattern,
+	isAssignmentPattern,
+	isBooleanLiteral,
+	isClassMemberModifier,
+	isEntityNameExpression,
+} from "./union";
+
+describe("isAccessExpression", () => {
+	it.each([
+		[false, `abc`],
+		[false, `abc.def()`],
+		[true, `abc.def`],
+		[true, `element['property'];`],
+	])("returns %j when given %s", (expected, sourceText) => {
+		expect(isAccessExpression(createNode(sourceText))).toBe(expected);
+	});
+});
+
+describe("isAccessibilityModifier", () => {
+	it.each([
+		[false, `abc`],
+		[true, ts.factory.createModifier(ts.SyntaxKind.PrivateKeyword)],
+		[true, ts.factory.createModifier(ts.SyntaxKind.ProtectedKeyword)],
+		[true, ts.factory.createModifier(ts.SyntaxKind.PublicKeyword)],
+	])("returns %j when given %s", (expected, sourceText) => {
+		expect(isAccessibilityModifier(createNode(sourceText))).toBe(expected);
+	});
+});
+
+describe("isArrayBindingOrAssignmentPattern", () => {
+	it.each([
+		[false, `"[a]"`],
+		[false, `"[a] = b"`],
+		[true, ts.factory.createArrayBindingPattern([])],
+		[true, ts.factory.createArrayLiteralExpression()],
+	])("returns %j when given %s", (expected, sourceText) => {
+		expect(isArrayBindingOrAssignmentPattern(createNode(sourceText))).toBe(
+			expected
+		);
+	});
+});
+
+describe("isAssignmentPattern", () => {
+	it.each([
+		[false, `"[a]"`],
+		[false, `"{a}"`],
+		[false, `"[a] = b"`],
+		[false, `"{a} = b"`],
+		[true, ts.factory.createObjectLiteralExpression()],
+		[true, ts.factory.createArrayLiteralExpression()],
+	])("returns %j when given %s", (expected, sourceText) => {
+		expect(isAssignmentPattern(createNode(sourceText))).toBe(expected);
+	});
+});
+
+describe("isBooleanLiteral", () => {
+	it.each([
+		[false, `boolean`],
+		[true, "false"],
+		[true, "true"],
+	])("returns %j when given %s", (expected, sourceText) => {
+		expect(isBooleanLiteral(createNode(sourceText))).toBe(expected);
+	});
+});
+
+describe("isClassMemberModifier", () => {
+	it.each([
+		[false, `boolean`],
+		[false, ts.factory.createModifier(ts.SyntaxKind.AbstractKeyword)],
+		[true, ts.factory.createModifier(ts.SyntaxKind.PrivateKeyword)],
+		[true, ts.factory.createModifier(ts.SyntaxKind.ReadonlyKeyword)],
+		[true, ts.factory.createModifier(ts.SyntaxKind.ReadonlyKeyword)],
+	])("returns %j when given %s", (expected, sourceText) => {
+		expect(isClassMemberModifier(createNode(sourceText))).toBe(expected);
+	});
+});
 
 describe("isEntityNameExpression", () => {
 	it.each([
