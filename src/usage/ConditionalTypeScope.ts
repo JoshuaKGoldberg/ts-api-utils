22b9802dd33b6b21f93719ddd92187d13e602a87
Josh Goldberg <git@joshuakgoldberg.com>
2023-09-06 01:27:42 -0400

    Added missing licenses

--- src/usage/ConditionalTypeScope.ts
+++ src/usage/ConditionalTypeScope.ts
@@ -1,3 +1,6 @@
+// Code largely based on https://github.com/ajafff/tsutils
+// Original license: https://github.com/ajafff/tsutils/blob/26b195358ec36d59f00333115aa3ffd9611ca78b/LICENSE
+
 import { NonRootScope } from "./NonRootScope";
 import { Scope, ScopeBoundary } from "./Scope";
 import { VariableUse } from "./variables";
--- src/usage/collectVariableUsage.ts
+++ src/usage/collectVariableUsage.ts
@@ -1,3 +1,6 @@
+// Code largely based on https://github.com/ajafff/tsutils
+// Original license: https://github.com/ajafff/tsutils/blob/26b195358ec36d59f00333115aa3ffd9611ca78b/LICENSE
+
 import ts from "typescript";
 
 import { UsageWalker } from "./UsageWalker";
