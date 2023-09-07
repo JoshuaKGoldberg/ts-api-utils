d6b5f18d13c1b02ec89a108f624e1ddbdde92256
Josh Goldberg <git@joshuakgoldberg.com>
2023-09-06 23:39:55 -0400

    getDecorators too

--- src/usage/utils.ts
+++ src/usage/utils.ts
@@ -13,10 +13,25 @@ export function identifierToKeywordKind(
 }
 
 /**
- * Supports TypeScript<4.8 versions that don't have identifierToKeywordKind.
+ * Supports TypeScript<4.8 versions that don't have canHaveDecorators.
  */
 export function canHaveDecorators(node: ts.Node): node is ts.HasDecorators {
 	return "canHaveDecorators" in ts
 		? ts.canHaveDecorators(node)
 		: "decorators" in node;
+}
+
+type NodeWithDecorators = ts.HasDecorators & {
+	decorators: readonly ts.Decorator[] | undefined;
+};
+
+/**
+ * Supports TypeScript<4.8 versions that don't have getDecorators.
+ */
+export function getDecorators(
+	node: ts.HasDecorators,
+): readonly ts.Decorator[] | undefined {
+	return "getDecorators" in ts
+		? ts.getDecorators(node)
+		: (node as NodeWithDecorators).decorators;
 }
