4f496157ef675cdca40397bf03c34f995653712c
Josh Goldberg <git@joshuakgoldberg.com>
2024-02-22 09:53:47 -0500

    Added some more testing

--- src/nodes/access.test.ts
+++ src/nodes/access.test.ts
@@ -1,28 +1,72 @@
 import ts from "typescript";
 import { describe, expect, it } from "vitest";
 
-import { createNodeAndSourceFile } from "../test/utils";
+import { createNode, createNode } from "../test/utils";
 import { AccessKind, getAccessKind } from "./access";
+
+function createNodeAndToString<Node extends ts.Node>(sourceText: string) {
+	const node = createNode<Node>(sourceText);
+	return {
+		node,
+		toString: () => node.getText(),
+	};
+}
 
 describe("getAccessKind", () => {
 	it("returns AccessKind.None when the node is not an access", () => {
-		const { node } = createNodeAndSourceFile<ts.Expression>("let value;");
+		const node = createNode<ts.Expression>("let value;");
 
 		const actual = getAccessKind(node);
 
 		expect(actual).toBe(AccessKind.None);
 	});
 
-	it("returns AccessKind.Read when the node is a read", () => {
-		const { node } = createNodeAndSourceFile<ts.Expression>("abc.def;");
+	it("returns AccessKind.Read when the node is a property access", () => {
+		const node = createNode<ts.Expression>("abc.def;");
 
 		const actual = getAccessKind(node);
 
 		expect(actual).toBe(AccessKind.Read);
 	});
 
-	it("returns AccessKind.Write when the node is a write", () => {
-		const { node } = createNodeAndSourceFile<ts.BinaryExpression>("abc = ghi;");
+	it("returns AccessKind.Read when the node is a shorthand property assignment in a variable", () => {
+		const node = createNode<ts.VariableStatement>("const abc = { def };");
+
+		const actual = getAccessKind(
+			node.declarationList.declarations[0].initializer!,
+		);
+
+		expect(actual).toBe(AccessKind.Read);
+	});
+
+	it("returns AccessKind.Read when the node is an array spread element", () => {
+		const node = createNode<ts.ArrayLiteralExpression>("[...abc]");
+
+		const actual = getAccessKind(node.elements[0]);
+
+		expect(actual).toBe(AccessKind.Read);
+	});
+
+	it("returns AccessKind.Read when the node is a nested array spread element", () => {
+		const node = createNode<
+			ts.ArrayLiteralExpression & {
+				elements: [
+					ts.SpreadElement & {
+						expression: ts.ArrayLiteralExpression & {
+							elements: ts.SpreadElement;
+						};
+					},
+				];
+			}
+		>("[...[...abc]]");
+
+		const actual = getAccessKind(node.elements[0].expression.elements[0]);
+
+		expect(actual).toBe(AccessKind.Read);
+	});
+
+	it("returns AccessKind.Write when the node is a binary assignment", () => {
+		const node = createNode<ts.BinaryExpression>("abc = ghi;");
 
 		const actual = getAccessKind(node.left);
 
@@ -30,19 +74,33 @@ describe("getAccessKind", () => {
 	});
 
 	it("returns AccessKind.Delete when the node is a delete", () => {
-		const { node } =
-			createNodeAndSourceFile<ts.DeleteExpression>("delete abc.def;");
+		const node = createNode<ts.DeleteExpression>("delete abc.def;");
 
 		const actual = getAccessKind(node.expression);
 
 		expect(actual).toBe(AccessKind.Delete);
 	});
 
-	it("returns AccessKind.ReadWrite when the node reads and writes", () => {
-		const { node } =
-			createNodeAndSourceFile<ts.BinaryExpression>("abc.def += 1;");
+	it("returns AccessKind.ReadWrite when the node is a binary read-write operator", () => {
+		const node = createNode<ts.BinaryExpression>("abc.def += 1;");
 
 		const actual = getAccessKind(node.left);
+
+		expect(actual).toBe(AccessKind.ReadWrite);
+	});
+
+	it("returns AccessKind.ReadWrite when the node is a postfix unary expression", () => {
+		const node = createNode<ts.PostfixUnaryExpression>("abc++;");
+
+		const actual = getAccessKind(node.operand);
+
+		expect(actual).toBe(AccessKind.ReadWrite);
+	});
+
+	it("returns AccessKind.ReadWrite when the node is a prefix unary expression", () => {
+		const node = createNode<ts.PostfixUnaryExpression>("++abc++");
+
+		const actual = getAccessKind(node.operand);
 
 		expect(actual).toBe(AccessKind.ReadWrite);
 	});
