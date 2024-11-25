57ede6cc741f11d0e0f3c9830e623e846b6fa2ab
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2024-11-25 16:06:14 +1300

    build: update entry file of tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	clean: true,
 	dts: true,
-	entry: ["src/**/*.ts", "!src/**/*.test.*"],
+	entry: ["src/index.ts"],
 	format: ["cjs", "esm"],
 	outDir: "lib",
 	sourcemap: true,
